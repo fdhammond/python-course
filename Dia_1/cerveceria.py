0c1f33abc11531a6edaa3f21934b084815fdebf4
fdhammond <federico.hammond8@gmail.com>
2024-12-28 13:00:55 -0300

    add first exercises

---
+++ Dia_1/cerveceria.py
@@ -0,0 +1,4 @@
+response1 = input("De que color te gusta la cerveza? ")
+response2 = input("De que parte del pais sos? Norte, sur, Este, Oeste? ")
+
+print(f"La marca de tu cerveza es:\n {response1} " + "" + f"{response2}")
