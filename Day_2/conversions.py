4cb30589425b6eab5455a6d69bfdd79bb434925b
fd <federico.hammond8@gmail.com>
2024-12-28 23:48:47 -0300

    add new exercises day 2

---
+++ Day_2/conversions.py
@@ -0,0 +1,24 @@
+## Exercise 1
+num1 = 7.5
+print(int(num1))
+
+## Exercise 2
+num1 = "7.5"
+num2 = "10"
+print(float(num1) + int(num2))
+
+## Exercise 3
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print(f"Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}")
+
+## Exercise 4
+puntos_nuevos = 350
+puntos_totales = 1225
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
+
+## Exercise 5
+puntos_anteriores = 875
+puntos_nuevos = 350
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_anteriores + puntos_nuevos } puntos")
---
+++ Day_2/operators.py
@@ -0,0 +1,13 @@
+## Exercise 1 - Floor division
+num1 = 874
+num2 = 27
+print(num1//num2)
+
+## Exercise 2 - Module
+num3 = 456
+num4 = 33
+print(num3 % num4)
+
+## Exercise 3 - Square root
+num6 = 783
+print(num6**0.5)
